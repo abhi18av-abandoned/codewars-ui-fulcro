8d77e07cc7f89d91303e95cd8e09bca2aa3ca779
Abhinav Sharma <abhi18av@gmail.com>
2019-10-16 13:19:59 +0530

    explore the basic api

--- src/app/external_api/codewars.cljs
+++ src/app/external_api/codewars.cljs
@@ -1,3 +1,48 @@
-(ns app.external-api.codewars)
+(ns ^{:author "Abhinav Sharma (@abhi18av)"
+      :doc    "Wraps the api for https://dev.codewars.com/"}
+
+  app.external-api.codewars
+
+  (:require [app.secrets :as secrets]
+            [app.utils :as utils :refer [namespaced-keys pull-namespaced pull-key update-if]]
+            [clojure.core.async :refer [go timeout <! take!]]
+            [clojure.string :as str]
+            [com.wsscode.common.async-cljs :refer [go-catch <? let-chan chan? <?maybe <!maybe go-promise]]
+            [com.wsscode.pathom.diplomat.http.fetch :as p.http.fetch]
+            [com.wsscode.pathom.core :as p]
+            [com.wsscode.pathom.connect :as pc]
+            [com.wsscode.pathom.connect.graphql :as pcg]
+            [com.wsscode.pathom.diplomat.http :as p.http]))
 
 
+
+;;============= scratch =======================
+
+
+
+(comment
+
+  (def memory (atom {}))
+
+  (defn api [{::keys [endpoint]}]
+    (let [cors-proxy "https://cors-anywhere.herokuapp.com/"
+          api-url "https://www.codewars.com/api/v1"]
+      (take!
+        (p.http.fetch/request-async {::p.http/url     (str cors-proxy api-url endpoint)
+                                     ::p.http/headers {:Authorization secrets/token}
+                                     ::p.http/as      ::p.http/json
+                                     ::p.http/method  "get"})
+        #(reset! memory (:com.wsscode.pathom.diplomat.http/body %)))))
+
+
+
+  @memory
+
+  (api {::endpoint "/users/abhi18av"})
+
+
+
+
+
+
+  '())
